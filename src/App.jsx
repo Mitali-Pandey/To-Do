e5c93911f435642973a6ebe480ea945dc622b37a
Mitali Pandey <mitalipandey498@gmail.com>
2025-12-07 12:44:37 +0530

    Revert " to do"
    This reverts commit 1ee0f9a27b473f58ef20851264cb9c7d2ba2fb82.

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,13 @@ import Navbar from './components/Navbar.jsx'
 import HomePage from './components/HomePage.jsx'
 import Footer from './components/Footer.jsx'
 import About from './components/About.jsx'
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 
 function App() {
   return (
     <>
-<Router>
-  <Navbar />
-  <Routes>
-    <Route path='/' element={<HomePage />} />
-  </Routes>
-</Router>
-    
-    {/* <About /> */}
+    <Navbar />
+    <About />
     {/* <HomePage /> */}
     <Footer />
     </>
